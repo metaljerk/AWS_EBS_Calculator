519b7d40dafb86705a25040a0cb53910f43af4bd
metaljerk <scottlonghofer@gmail.com>
2022-05-28 10:41:42 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder="templates")
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-            return render_template("index.html")
+        return render_template("index.html")
     if request.method == "POST":
         error = False
         error = (
